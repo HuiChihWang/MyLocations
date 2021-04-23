58a8ef3fd7457c70143ba960830ac7b78764e481
Gilbert <taya87136@.gmail.com>
2021-04-23 20:55:30 +0800

    update location and address
    location update has some problem in moving object

--- LocationHandler.swift
+++ LocationHandler.swift
@@ -7,26 +7,126 @@
 
 import Foundation
 import CoreLocation
+import Contacts
 
-class LocationHandler {
-    let locationManager = CLLocationManager()
+class LocationHandler: NSObject {
     
-    init() {
-        setupLocationManager()
-    }
-    
-    public func getCurrentLocation() -> CLLocation {
-        locationManager.startUpdatingLocation()
-        return locationManager.location ?? CLLocation()
-    }
-    
-    private func setupLocationManager() {
-//        locationManager.delegate = self
+    lazy var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+        locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        return locationManager
+    }()
+    
+    lazy var geoCoder = CLGeocoder()
+    
+    var currentLocation: CLLocation?
+    var address: String?
+    private var isAddressUpdated = false
+    
+    var status: LocationStatus = .initial
+    
+    public func initializeSearch() {
+        requestAuthorization()
+        status = .searching
+    }
+    
+    public func searchLocation() {
+        guard status == .searching else {
+            return
+        }
+        
+        locationManager.startUpdatingLocation()
+        updateLocation()
+        locationManager.stopUpdatingLocation()
+        
+        if let location = currentLocation {
+            transformLocationToAddress(with: location)
+            while !isAddressUpdated { }
+        }
+    }
+    
+    private func updateLocation() {
+        while status == .searching {
+            if let newLocation = locationManager.location {
+                
+                if newLocation.timestamp.timeIntervalSinceNow < -5 {
+                    continue
+                }
+                
+                if newLocation.horizontalAccuracy < 0 {
+                    continue
+                }
+                
+                if newLocation.horizontalAccuracy < locationManager.desiredAccuracy {
+                    print("Updated")
+                    currentLocation = newLocation
+                    status = .updated
+                }
+            }
+        }
+    }
+    
+    private func requestAuthorization() {
+        if locationManager.authorizationStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    private func transformLocationToAddress(with location: CLLocation) {
+        isAddressUpdated = false
+        geoCoder.reverseGeocodeLocation(location) { [weak self] placeMarks, error in
+            self?.address = placeMarks?.last?.formattedAddress
+            self?.isAddressUpdated = true
+        }
+        
     }
 }
 
-//extension LocationHandler: CLLocationManagerDelegate {
-//}
+extension LocationHandler: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("didFailWithError \(error.localizedDescription)")
+        
+        if (locationManager.authorizationStatus == .notDetermined) {
+            return
+        }
+        switch (error as NSError).code {
+        case CLError.locationUnknown.rawValue:
+            status = .unknown
+        case CLError.denied.rawValue:
+            status = .disabled
+        case CLError.network.rawValue:
+            status = .networkIssue
+        default:
+            status = .initial
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("did update locations \(String(describing: locations.last))")
+    }
+}
+
+extension CLPlacemark {
+    var formattedAddress: String {
+        guard postalAddress != nil else {
+            return "UnknownPlace"
+        }
+        
+        let formatter = CNPostalAddressFormatter()
+        var address = formatter.string(from: postalAddress!)
+        address = address.replacingOccurrences(of: "\n", with: ", ")
+        return address
+    }
+}
+enum LocationStatus: String {
+    case searching = "Searching..."
+    case disabled = "Location Services Disabled"
+    case updated = "Current Location"
+    case unknown = "Error Getting Location"
+    case networkIssue = "Please Turn on Mobile Network"
+    case initial = "Tap 'Get My Location' to Start"
+}
 
 
--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class CurrentLocationViewController: UIViewController {
 
@@ -13,6 +14,7 @@ class CurrentLocationViewController: UIViewController {
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
     
     let locationHelper = LocationHandler()
     
@@ -22,9 +24,47 @@ class CurrentLocationViewController: UIViewController {
     }
 
     @IBAction func updateLocation(_ sender: Any) {
-        let location = locationHelper.getCurrentLocation()
+        locationHelper.initializeSearch()
+        updateView()
+        
+        DispatchQueue.global().async {
+            
+            self.locationHelper.searchLocation()
+            
+            DispatchQueue.main.async {
+                self.updateView()
+            }
+            
+            self.locationHelper.searchLocation()
+        }
+    }
+    
+    private func updateView() {
+        configureLocationView(with: locationHelper.currentLocation ?? CLLocation(), at: locationHelper.address ?? "Unknow Address")
+        updateMessageStatus(with: locationHelper.status)
+    }
+    private func configureLocationView(with location: CLLocation, at address: String) {
         latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
         longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
+        addressLabel.text = address
+    }
+    
+    private func updateMessageStatus(with status: LocationStatus) {
+        messageLabel.text = status.rawValue
+        tagButton.isHidden = status != .updated
+        
+        if (status == .disabled) {
+            showDisabledAlert()
+        }
+    }
+    
+    private func showDisabledAlert() {
+        let alert = UIAlertController(title: "Location Service Disabled", message: "Please enable location services for this app in Settings.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
     
 }
